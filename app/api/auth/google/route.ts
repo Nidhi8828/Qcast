37b4e2dd4705766529de19e2d641b934b2aa93e6
Dhanashree <dhanashreegosavi183@gmail.com>
2025-02-19 16:12:19 +0530

    fixed google sign in route

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -1,7 +1,14 @@
-'use server';
+// 'use server';
+
+// import { signIn } from '@/lib/auth';
+
+// export async function googleSignIn() {
+//   await signIn('google', { redirectTo: '/' });
+// }
 
 import { signIn } from '@/lib/auth';
+import { NextResponse } from 'next/server';
 
-export async function googleSignIn() {
-  await signIn('google', { redirectTo: '/' });
+export async function GET() {
+  return signIn('google', { redirectTo: '/' });
 }
